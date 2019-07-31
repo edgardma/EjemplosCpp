d039b6cac25ce9d1b7f76a25b345fa4072acf18f
Edgard Marquez <edgardma@gmail.com>
2019-07-30 22:13:49 -0500

    Actualizar archivo .gitignore

---
+++ game/cmake-build-debug/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/game.cpp.o"
+  "CMakeFiles/game.dir/Automovil.cpp.o"
+  "CMakeFiles/game.dir/Persona.cpp.o"
+  "CMakeFiles/game.dir/Rueda.cpp.o"
+  "CMakeFiles/game.dir/Vehiculo.cpp.o"
+  "game.pdb"
+  "game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
